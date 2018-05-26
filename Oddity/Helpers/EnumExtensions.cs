7071917d9758fc2a04cba56cce5b32d708d4c595
tearth <Tearth@users.noreply.github.com>
2018-05-27 00:27:13 +0200

    Change enum extension to be compatible with lower .NET Standard versions.

--- Oddity/Helpers/EnumExtensions.cs
+++ Oddity/Helpers/EnumExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization;
 
 namespace Oddity.Helpers
@@ -8,9 +9,9 @@ namespace Oddity.Helpers
     {
         public static string GetEnumMemberAttributeValue(this Enum enumObject, object enumValue)
         {
-            var enumType = enumObject.GetType();
+            var enumType = enumObject.GetType().GetTypeInfo();
 
-            var memberInfo = enumType.GetMember(enumValue.ToString())[0];
+            var memberInfo = enumType.GetDeclaredField(enumValue.ToString());
             var enumMemberAttribute = memberInfo.GetCustomAttributes(false).OfType<EnumMemberAttribute>().FirstOrDefault();
 
             return enumMemberAttribute?.Value;
