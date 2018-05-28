7b760d72bbb2bdbcb33fb10ef45cd116e06d7548
tearth <Tearth@users.noreply.github.com>
2018-05-28 23:08:07 +0200

    Add detailed capsule info model.

---
+++ Oddity/API/Models/DetailedCapsule/DetailedCapsuleInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Oddity.API.Models.Capsule;
+
+namespace Oddity.API.Models.DetailedCapsule
+{
+    public class DetailedCapsuleInfo
+    {
+        [JsonProperty("capsule_serial")]
+        public string CapsuleSerial { get; set; }
+
+        [JsonProperty("capsule_id")]
+        public CapsuleId CapsuleId { get; set; }
+
+        public string Status { get; set; }
+
+        [JsonProperty("original_launch")]
+        public DateTime OriginalLaunch { get; set; }
+
+        public List<String> Missions { get; set; }
+        public int Landings { get; set; }
+        public string Type { get; set; }
+        public string Details { get; set; }
+    }
+}
