e93ad9a4d7c87fc10ed467c4fb99c38c38371f07
tearth <Tearth@users.noreply.github.com>
2018-05-29 13:24:48 +0200

    Add model for detailed core info.

---
+++ Oddity/API/Models/DetailedCore/DetailedCoreInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Oddity.API.Models.DetailedCore
+{
+    public class DetailedCoreInfo
+    {
+        [JsonProperty("core_serial")]
+        public string CoreSerial { get; set; }
+
+        public int? Block { get; set; }
+        public DetailedCoreStatus? Status { get; set; }
+
+        [JsonProperty("original_launch")]
+        public string OriginalLaunch { get; set; }
+
+        public List<string> Missions { get; set; }
+
+        [JsonProperty("rtls_attempt")]
+        public bool? RtlcAttempt { get; set; }
+
+        [JsonProperty("rtls_landings")]
+        public int? RtlcLandings { get; set; }
+
+        [JsonProperty("asds_attempt")]
+        public bool? AsdsAttempt { get; set; }
+
+        [JsonProperty("asds_landings")]
+        public int? AsdsLandings { get; set; }
+
+        [JsonProperty("water_landing")]
+        public bool? WaterLanding { get; set; }
+
+        public string Details { get; set; }
+    }
+}
---
+++ Oddity/API/Models/DetailedCore/DetailedCoreStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Oddity.API.Models.DetailedCore
+{
+    public enum DetailedCoreStatus
+    {
+        [EnumMember(Value = "active")]
+        Active,
+
+        [EnumMember(Value = "destroyed")]
+        Destroyed,
+
+        [EnumMember(Value = "retired")]
+        Retired,
+
+        [EnumMember(Value = "unknown")]
+        Unknown,
+    }
+}
