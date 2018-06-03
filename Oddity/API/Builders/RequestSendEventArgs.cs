a4d4ce0d81febcc15438f4dcd26ab9b9675180aa
tearth <Tearth@users.noreply.github.com>
2018-06-03 18:05:53 +0200

    Add event arguments for OnResponseReceive and OnRequestSend events.

---
+++ Oddity/API/Builders/RequestSendEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oddity.API.Builders
+{
+    /// <summary>
+    /// Contains OnRequestSend event arguments.
+    /// </summary>
+    public class RequestSendEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the URL which has been called to retrieve the specified data.
+        /// </summary>
+        public string Url { get; }
+
+        /// <summary>
+        /// Gets the dictionary of filters which has been applied.
+        /// </summary>
+        public Dictionary<string, string> Filteres { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestSendEventArgs"/> class.
+        /// </summary>
+        /// <param name="url">The URL which has been called to retrieve the specified data.</param>
+        /// <param name="filters">The dictionary of filters which has been applied.</param>
+        public RequestSendEventArgs(string url, Dictionary<string, string> filters)
+        {
+            Url = url;
+            Filteres = filters;
+        }
+    }
+}
