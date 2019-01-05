d8ae3bd3a01729ef9159ed588c8e0b5aae5df87f
Martin Evans <martindevans@gmail.com>
2019-01-05 22:23:54 +0000

    - Added TentativeMaxPrecision enum to represent how precise the launch date is.  - Added `IsTentative` and `TentativeMaxPrecision` fields to `LaunchInfo`

---
+++ Oddity/API/Models/Launch/TentativeMaxPrecision.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace Oddity.API.Models.Launch
+{
+    public enum TentativeMaxPrecision
+    {
+        [EnumMember(Value = "hour")]
+        Hour,
+
+        [EnumMember(Value = "day")]
+        Day,
+
+        [EnumMember(Value = "month")]
+        Month,
+
+        [EnumMember(Value = "quarter")]
+        Quarter,
+
+        [EnumMember(Value = "half")]
+        Half,
+
+        [EnumMember(Value = "year")]
+        Year
+    }
+}
