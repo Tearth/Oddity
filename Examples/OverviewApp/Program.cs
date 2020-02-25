081effbfb5dd829e0884e7052764c08c58f684f7
tearth <tearthdev@gmail.com>
2020-02-25 17:43:02 +0100

    Fix style issues in example app.

--- Examples/OverviewApp/Program.cs
+++ Examples/OverviewApp/Program.cs
@@ -13,7 +13,7 @@ namespace OverviewApp
         {
             var oddity = new OddityCore();
 
-            // Optional.
+            // Optional
             oddity.OnDeserializationError += OddityOnDeserializationError;
             oddity.OnRequestSend += Oddity_OnRequestSend;
             oddity.OnResponseReceive += OddityOnResponseReceive;
@@ -21,40 +21,40 @@ namespace OverviewApp
             // Get API info
             var api = oddity.Api.GetInfo().Execute();
 
-            // Get company information.
+            // Get company information
             var company = oddity.Company.GetInfo().Execute();
 
-            // Get all history.
+            // Get all history
             var history = oddity.Company.GetHistory().Execute();
 
-            // Get history from the last two years and ordered descending.
+            // Get history from the last two years and ordered descending
             var historyWithFilter = oddity.Company.GetHistory().WithRange(DateTime.Now.AddYears(-2), DateTime.Now).Descending().Execute();
 
-            // Get data about Falcon Heavy.
+            // Get data about Falcon Heavy
             var falconHeavy = oddity.Rockets.GetAbout(RocketId.FalconHeavy).Execute();
 
-            // Get list of all launchpads.
+            // Get list of all launchpads
             var allLaunchpads = oddity.Launchpads.GetAll().Execute();
 
-            // Get all launches.
+            // Get all launches
             var allLaunches = oddity.Launches.GetAll().Execute();
 
-            // Get information about the next launch.
+            // Get information about the next launch
             var nextLaunch = oddity.Launches.GetNext().Execute();
 
-            // Get data about all launches of Falcon 9 which has been launched to ISS and landed with success. Next, sort it ascending.
+            // Get data about all launches of Falcon 9 which has been launched to ISS and landed with success. Next, sort it ascending
             var launchWithFilters = oddity.Launches.GetAll().WithRocketName("Falcon 9").WithOrbit(OrbitType.ISS).Ascending().Execute();
 
-            // Get all capsule types.
+            // Get all capsule types
             var capsuleTypes = oddity.Capsules.GetAll().Execute();
 
-            // Get capsule which has been launched 2015-04-14 at 20:10.
+            // Get capsule which has been launched 2015-04-14 at 20:10
             var capsuleWithFilters = oddity.DetailedCapsules.GetAll().WithOriginalLaunch(new DateTime(2015, 4, 14, 20, 10, 0)).Execute();
 
-            // Get all cores.
+            // Get all cores
             var allCores = oddity.DetailedCores.GetAll().Execute();
 
-            // Get Roadster info.
+            // Get Roadster info
             var roadster = oddity.Roadster.Get().Execute();
 
             Console.Read();
@@ -64,7 +64,7 @@ namespace OverviewApp
         {
             Console.WriteLine("Something went wrong.");
 
-            // We don't want to stop program, just leave problematic field as null.
+            // We don't want to stop program, just leave problematic field as null
             errorEventArgs.ErrorContext.Handled = true;
         }
 
