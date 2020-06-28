b38af5aee5b6eb434958e7fc3daf5754caab523b
Tearth <tearthdev@gmail.com>
2020-06-28 17:40:53 +0200

    Add missing data to company info

---
+++ Oddity/API/Models/Company/CompanyLinks.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace Oddity.API.Models.Company
+{
+    public class CompanyLinks
+    {
+        public string Website { get; set; }
+        public string Flickr { get; set; }
+        public string Twitter { get; set; }
+
+        [JsonProperty("elon_twitter")]
+        public string ElonTwitter { get; set; }
+    }
+}
