0d68a880bdeb2ec51b75324d44f9c44d60c48b08
Tearth <tearthdev@gmail.com>
2020-07-10 01:10:51 +0200

    Add builder base

---
+++ Oddity/API/Builders/BuilderBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Oddity.API.Exceptions;
+
+namespace Oddity.API.Builders
+{
+    /// <summary>
+    /// Represents an abstract base class for all builders.
+    /// </summary>
+    public abstract class BuilderBase<TReturn>
+    {
+        protected readonly BuilderDelegatesContainer BuilderDelegatesContainer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuilderBase{TReturn}"/> class.
+        /// </summary>
+        /// <param name="builderDelegatesContainer">The builder delegates container.</param>
+        protected BuilderBase(BuilderDelegatesContainer builderDelegatesContainer)
+        {
+            BuilderDelegatesContainer = builderDelegatesContainer;
+        }
+
+        /// <summary>
+        /// Performs an request to the API with the specified parameters (if specified earlier).
+        /// </summary>
+        /// <returns>The all capsules information or null/empty list if object is not available.</returns>
+        /// <exception cref="APIUnavailableException">Thrown when SpaceX API is unavailable.</exception>
+        public abstract TReturn Execute();
+
+        /// <summary>
+        /// Performs an async request to the API with the specified parameters (if specified earlier).
+        /// </summary>
+        /// <returns>The all capsules information or null/empty list if object is not available.</returns>
+        /// <exception cref="APIUnavailableException">Thrown when SpaceX API is unavailable.</exception>
+        public abstract Task<TReturn> ExecuteAsync();
+
+        protected TReturn DeserializeJson(string content)
+        {
+            var deserializationSettings = new JsonSerializerSettings { Error = JsonDeserializationError };
+            return JsonConvert.DeserializeObject<TReturn>(content, deserializationSettings);
+        }
+
+        private void JsonDeserializationError(object sender, ErrorEventArgs errorEventArgs)
+        {
+            BuilderDelegatesContainer.DeserializationError(errorEventArgs);
+        }
+    }
+}
---
+++ Oddity/API/Builders/SimpleBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Oddity.API.Exceptions;
+
+namespace Oddity.API.Builders
+{
+    public class SimpleBuilder<TReturn> : BuilderBase<TReturn>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _endpoint;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleBuilder{TReturn}"/> class.
+        /// </summary>
+        /// <param name="httpClient">The HTTP client.</param>
+        /// <param name="endpoint">The endpoint used in this instance to retrieve data from API.</param>
+        /// <param name="builderDelegates">The builder delegates container.</param>
+        public SimpleBuilder(HttpClient httpClient, string endpoint, BuilderDelegatesContainer builderDelegates) : base(builderDelegates)
+        {
+            _httpClient = httpClient;
+            _endpoint = endpoint;
+        }
+
+        /// <inheritDoc />
+        public override TReturn Execute()
+        {
+            return ExecuteAsync().GetAwaiter().GetResult();
+        }
+
+        /// <inheritDoc />
+        public override async Task<TReturn> ExecuteAsync()
+        {
+            BuilderDelegatesContainer.RequestSend(new RequestSendEventArgs(_httpClient.BaseAddress + _endpoint));
+
+            var response = await _httpClient.GetAsync(_endpoint).ConfigureAwait(false);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                if (response.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return default;
+                }
+
+                throw new APIUnavailableException($"Status code: {(int)response.StatusCode}");
+            }
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            var eventArgs = new ResponseReceiveEventArgs(content, response.StatusCode, response.ReasonPhrase);
+            BuilderDelegatesContainer.ResponseReceived(eventArgs);
+
+            return DeserializeJson(content);
+        }
+    }
+}
