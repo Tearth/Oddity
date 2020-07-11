351dbf5b7a52d49660c53f90b4599e5db344772a
Tearth <tearthdev@gmail.com>
2020-07-11 19:07:27 +0200

    Add context setting in nested objects

--- Oddity/API/Models/ModelBase.cs
+++ Oddity/API/Models/ModelBase.cs
@@ -1,4 +1,7 @@
-﻿namespace Oddity.API.Models
+﻿using System;
+using System.Reflection;
+
+namespace Oddity.API.Models
 {
     public abstract class ModelBase
     {
@@ -7,6 +10,25 @@
         public void SetContext(OddityCore context)
         {
             Context = context;
+            SetContextInNestedObjects(context);
+        }
+
+        private void SetContextInNestedObjects(OddityCore context)
+        {
+            foreach (var property in GetType().GetRuntimeProperties())
+            {
+                var propertyTypeInfo = property.PropertyType.GetTypeInfo();
+                if (propertyTypeInfo.IsSubclassOf(typeof(ModelBase)))
+                {
+                    var underlyingInstance = property.GetMethod.Invoke(this, null);
+                    if (underlyingInstance != null)
+                    {
+                        var baseTypeInfo = propertyTypeInfo.BaseType.GetTypeInfo();
+                        var setContextMethod = baseTypeInfo.GetDeclaredMethod("SetContext");
+                        setContextMethod.Invoke(underlyingInstance, new object[] { context });
+                    }
+                }
+            }
         }
     }
 }
