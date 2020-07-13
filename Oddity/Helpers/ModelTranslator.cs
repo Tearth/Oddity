5c53c1e874829a2aeda4df7a30767acd573fcbb4
Tearth <tearthdev@gmail.com>
2020-07-13 14:35:11 +0200

    Add model translator

---
+++ Oddity/Helpers/ModelTranslator.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Oddity.Models;
+
+namespace Oddity.Helpers
+{
+    public static class ModelTranslator
+    {
+        private static Dictionary<string, string> _map;
+
+        static ModelTranslator()
+        {
+            InitializeMap();
+        }
+
+        public static string Map(string key)
+        {
+            return _map[key];
+        }
+
+        private static void InitializeMap()
+        {
+            _map = new Dictionary<string, string>();
+
+            var assembly = typeof(ModelTranslator).GetTypeInfo().Assembly;
+            var modelBaseClasses = assembly.DefinedTypes.Where(p => p.IsSubclassOf(typeof(ModelBase)));
+
+            foreach (var modelBaseClass in modelBaseClasses)
+            {
+                var properties = modelBaseClass.DeclaredProperties;
+                foreach (var property in properties)
+                {
+                    var jsonPropertyAttribute = property.CustomAttributes
+                        .FirstOrDefault(p => p.AttributeType == typeof(JsonPropertyAttribute));
+
+                    if (jsonPropertyAttribute != null && jsonPropertyAttribute.ConstructorArguments.Count > 0)
+                    {
+                        _map[property.Name] = (string)jsonPropertyAttribute.ConstructorArguments[0].Value;
+                    }
+                    else
+                    {
+                        _map[property.Name] = property.Name.ToLower();
+                    }
+                }
+            }
+        }
+    }
+}
