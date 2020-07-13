777b7a561ae846f2ad2a712b66a0be0ca8288770
Tearth <tearthdev@gmail.com>
2020-07-13 18:26:47 +0200

    Update example app to the new library version

--- Examples/UpcomingLaunchesApp/Program.cs
+++ Examples/UpcomingLaunchesApp/Program.cs
@@ -9,7 +9,6 @@ namespace UpcomingLaunchesApp
     {
         public static void Main(string[] args)
         {
-            /*
             var oddity = new OddityCore();
             oddity.OnDeserializationError += OddityOnOnDeserializationError;
 
@@ -17,32 +16,31 @@ namespace UpcomingLaunchesApp
             DisplayRestOfUpcomingLaunches(oddity);
 
             Console.Read();
-            */
         }
-        /*
+        
         private static void DisplayNextLaunch(OddityCore oddity)
         {
-            var nextLaunchData = oddity.Launches.GetNext().Execute();
+            var nextLaunchData = oddity.LaunchesEndpoint.GetNext().Execute();
 
             Console.WriteLine("Next launch data:");
             Console.WriteLine("---------------------------------------------------------------------------");
-            Console.WriteLine("Mission name     | " + nextLaunchData.MissionName);
-            Console.WriteLine("Launchpad        | " + nextLaunchData.LaunchSite.SiteLongName);
-            Console.WriteLine("Launch date UTC  | " + nextLaunchData.LaunchDateUtc);
-            Console.WriteLine("Rocket           | " + nextLaunchData.Rocket.RocketName);
-            Console.WriteLine("Payloads         | " + string.Join(',', nextLaunchData.Rocket.SecondStage.Payloads.Select(p => p.PayloadId)));
+            Console.WriteLine("Mission name     | " + nextLaunchData.Name);
+            Console.WriteLine("Launchpad        | " + nextLaunchData.Launchpad.Value.FullName);
+            Console.WriteLine("Launch date UTC  | " + nextLaunchData.DateUtc);
+            Console.WriteLine("Rocket           | " + nextLaunchData.Rocket.Value.Name);
+            Console.WriteLine("Payloads         | " + string.Join(',', nextLaunchData.Payloads.Select(p => p.Value.Name)));
             Console.WriteLine();
         }
 
         private static void DisplayRestOfUpcomingLaunches(OddityCore oddity)
         {
-            var upcomingLaunches = oddity.Launches.GetUpcoming().Execute();
+            var upcomingLaunches = oddity.LaunchesEndpoint.GetUpcoming().Execute();
 
             Console.WriteLine("All upcoming launches:");
             Console.WriteLine("---------------------------------------------------------------------------");
             foreach (var launch in upcomingLaunches)
             {
-                Console.WriteLine($"{launch.MissionName} ({launch.LaunchDateUtc}) at {launch.LaunchSite.SiteName}");
+                Console.WriteLine($"{launch.Name} ({launch.DateUtc}) at {launch.Launchpad.Value.FullName}");
             }
         }
 
@@ -51,6 +49,5 @@ namespace UpcomingLaunchesApp
             Console.WriteLine($"Error: {errorEventArgs.ErrorContext.Path}");
             errorEventArgs.ErrorContext.Handled = true;
         }
-        */
     }
 }
