151dee4d65590aac2b123714d48ddac961d9c4c3
Tearth <tearthdev@gmail.com>
2020-07-14 17:25:14 +0200

    Add pagination test to the example app

--- Examples/OverviewApp/Program.cs
+++ Examples/OverviewApp/Program.cs
@@ -2,7 +2,8 @@
 using System.Threading.Tasks;
 using Newtonsoft.Json.Serialization;
 using Oddity;
-using Oddity.API.Events;
+using Oddity.Events;
+using Oddity.Helpers;
 
 namespace OverviewApp
 {
@@ -17,16 +18,15 @@ namespace OverviewApp
             oddity.OnRequestSend += Oddity_OnRequestSend;
             oddity.OnResponseReceive += OddityOnResponseReceive;
 
+            // Test of the /capsules endpoint
             var allCapsules = await oddity.CapsulesEndpoint.GetAll().ExecuteAsync();
             var capsule = await oddity.CapsulesEndpoint.Get("5e9e2c5bf35918ed873b2664").ExecuteAsync();
             var capsuleLaunch = capsule.Launches[0].Value;
 
-            var allCrew = await oddity.CrewEndpoint.GetAll().ExecuteAsync();
-            var crewMember = await oddity.CrewEndpoint.Get("5ebf1b7323a9a60006e03a7b").ExecuteAsync();
-            var crewMemberLaunch = crewMember.Launches[0].Value;
-
+            // Test of the /company endpoint
             var company = await oddity.CompanyEndpoint.Get().ExecuteAsync();
 
+            // Test of the /cores endpoint
             var allCores = await oddity.CoresEndpoint.GetAll().ExecuteAsync();
             var core = await oddity.CoresEndpoint.Get("5e9e28a6f35918c0803b265c").ExecuteAsync();
             var coreLaunch1 = core.Launches[0].Value;
@@ -34,16 +34,24 @@ namespace OverviewApp
             var coreLaunch3 = core.Launches[2].Value;
             var coreLaunch4 = core.Launches[3].Value;
 
+            // Test of the /crew endpoint
+            var allCrew = await oddity.CrewEndpoint.GetAll().ExecuteAsync();
+            var crewMember = await oddity.CrewEndpoint.Get("5ebf1b7323a9a60006e03a7b").ExecuteAsync();
+            var crewMemberLaunch = crewMember.Launches[0].Value;
+
+            // Test of the /landpads endpoint
             var allLandpads = await oddity.LandpadsEndpoint.GetAll().ExecuteAsync();
             var landpad = await oddity.LandpadsEndpoint.Get("5e9e3032383ecb90a834e7c8").ExecuteAsync();
             var landpadLaunch = landpad.Launches[0].Value;
 
+            // Test of the /launches endpoint
             var allLaunches = await oddity.LaunchesEndpoint.GetAll().ExecuteAsync();
             var pastLaunches = await oddity.LaunchesEndpoint.GetPast().ExecuteAsync();
             var upcomingLaunches = await oddity.LaunchesEndpoint.GetUpcoming().ExecuteAsync();
             var latestLaunch = await oddity.LaunchesEndpoint.GetPast().ExecuteAsync();
             var nextLaunch = await oddity.LaunchesEndpoint.GetNext().ExecuteAsync();
 
+            // Test of the /crew endpoint
             var crewLaunch = await oddity.LaunchesEndpoint.Get("5eb87d46ffd86e000604b388").ExecuteAsync();
             var crewLaunchCore = crewLaunch.Cores[0].Core.Value;
             var crewLaunchLandpad = crewLaunch.Cores[0].Landpad.Value;
@@ -54,6 +62,7 @@ namespace OverviewApp
             var crewLaunchPayload = crewLaunch.Payloads[0].Value;
             var crewLaunchLaunchpad = crewLaunch.Launchpad.Value;
 
+            // Test of the /launches endpoint
             var commercialLaunch = await oddity.LaunchesEndpoint.Get("5eb87d46ffd86e000604b389").ExecuteAsync();
             var commercialLaunchCore = commercialLaunch.Cores[0].Core.Value;
             var commercialLaunchLandpad = commercialLaunch.Cores[0].Landpad.Value;
@@ -63,6 +72,7 @@ namespace OverviewApp
             var commercialLaunchPayload = commercialLaunch.Payloads[0].Value;
             var commercialLaunchLaunchpad = commercialLaunch.Launchpad.Value;
 
+            // Test of the /launchpads endpoint
             var allLaunchpads = await oddity.LaunchpadsEndpoint.GetAll().ExecuteAsync();
             var launchpad = await oddity.LaunchpadsEndpoint.Get("5e9e4502f509092b78566f87").ExecuteAsync();
             var launchpadLaunch1 = launchpad.Launches[0].Value;
@@ -71,16 +81,20 @@ namespace OverviewApp
             var launchpadLaunch4 = launchpad.Launches[3].Value;
             var launchpadRocket = launchpad.Rockets[0].Value;
 
+            // Test of the /payloads endpoint
             var allPayloads = await oddity.PayloadsEndpoint.GetAll().ExecuteAsync();
             var payload = await oddity.PayloadsEndpoint.Get("5eb0e4bbb6c3bb0006eeb1ed").ExecuteAsync();
             var payloadLaunch = payload.Launch.Value;
             var payloadCapsule = payload.Dragon.Capsule.Value;
 
+            // Test of the /roadster endpoint
             var roadster = await oddity.RoadsterEndpoint.Get().ExecuteAsync();
 
+            // Test of the /rockets endpoint
             var allRockets = await oddity.RocketsEndpoint.GetAll().ExecuteAsync();
             var rocket = await oddity.RocketsEndpoint.Get("5e9d0d95eda69974db09d1ed").ExecuteAsync();
 
+            // Test of the /ships endpoint
             var allShips = await oddity.ShipsEndpoint.GetAll().ExecuteAsync();
             var ship = await oddity.ShipsEndpoint.Get("5ea6ed2e080df4000697c90a").ExecuteAsync();
             var shipLaunch1 = ship.Launches[0].Value;
@@ -88,9 +102,25 @@ namespace OverviewApp
             var shipLaunch3 = ship.Launches[2].Value;
             var shipLaunch4 = ship.Launches[3].Value;
 
+            // Test of the /starlink endpoint
             var allStarlinks = await oddity.StarlinkEndpoint.GetAll().ExecuteAsync();
             var starlink = await oddity.StarlinkEndpoint.Get("5eed7716096e590006985825").ExecuteAsync();
             var starlinkLaunch = starlink.Launch.Value;
+
+            // Test of the pagination
+            var queryStarlink = await oddity.StarlinkEndpoint.Query()
+                .WithFieldGreaterThan("SpaceTrack.Apoapsis", 500)
+                .WithLimit(100)
+                .SortBy("SpaceTrack.Apoapsis", false)
+                .ExecuteAsync();
+            await queryStarlink.GoToNextPage();
+            await queryStarlink.GoToNextPage();
+            await queryStarlink.GoToNextPage();
+            await queryStarlink.GoToPrevPage();
+            await queryStarlink.GoToPrevPage();
+            await queryStarlink.GoToPrevPage();
+            await queryStarlink.GoToLastPage();
+            await queryStarlink.GoToFirstPage();
 
             Console.Read();
         }
@@ -106,6 +136,11 @@ namespace OverviewApp
         private static void Oddity_OnRequestSend(object sender, RequestSendEventArgs e)
         {
             Console.WriteLine($"Sending request... URL: {e.Url}");
+
+            if (e.Query != null)
+            {
+                Console.WriteLine($"Query: {e.Query}");
+            }
         }
 
         private static void OddityOnResponseReceive(object sender, ResponseReceiveEventArgs e)
