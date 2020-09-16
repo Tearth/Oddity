ac62c3cbd7a98da093f6690ab7f15f33f92dfd18
Tearth <tearthdev@gmail.com>
2020-09-16 14:55:35 +0200

    Update "failures" field in launch info

---
+++ Oddity/Models/Launches/LaunchFailure.cs
@@ -0,0 +1,9 @@
+﻿namespace Oddity.Models.Launches
+{
+    public class LaunchFailure : ModelBase
+    {
+        public int? Time { get; set; }
+        public uint? Altitude { get; set; }
+        public string Reason { get; set; }
+    }
+}
